5d29296cea7e22da3effa21cdcbffd0f1267d5aa
Juan J. Prieto <root@sdk7.redborder.lan>
2016-05-10 05:18:29 +0200

    new librb-http spec file

--- modules/librb-http/build.sh
+++ modules/librb-http/build.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
-VERSION=${VERSION:="1.1.2"}
+COMMIT=${COMMIT:="b5dc6128058c53b9be0f8244db143cd34cfb32fe"}
+VERSION=${VERSION:="1.2.0"}
 PACKNAME=${PACKNAME:="librb-http"}
 CACHEDIR=${CACHEDIR:="/tmp/sdk7_cache/custom_rpms"}
 REPODIR=${REPODIR:="/tmp/sdk7_repo"}
+VSHORT=$(c=${COMMIT}; echo ${c:0:7})
 
 # First we need to download source
 mkdir SOURCES
-wget --no-check-certificate https://gitlab.redborder.lan/core-developers/${PACKNAME}/repository/archive.tar.gz?ref=${VERSION} --header='PRIVATE-TOKEN:oDRezN5gFLgBB6nWsMZU' -O SOURCES/${PACKNAME}-${VERSION}.tar.gz
+wget --no-check-certificate https://gitlab.redborder.lan/core-developers/${PACKNAME}/repository/archive.tar.gz?ref=${VERSION} --header='PRIVATE-TOKEN:oDRezN5gFLgBB6nWsMZU' -O SOURCES/${PACKNAME}-${VERSION}-${VSHORT}.tar.gz
 
-## Now it is time to create the source rpm
-#/usr/bin/mock -r default --define "__version ${VERSION}" --define "__release 1" --resultdir=pkgs --buildsrpm --spec=${PACKNAME}.spec --sources=SOURCES
-#
-## with it, we can create rest of packages
-#/usr/bin/mock -r default --define "__version ${VERSION}" --define "__release 1" --resultdir=pkgs --rebuild pkgs/${PACKNAME}*.src.rpm
-#
-## cleaning
-#rm -rf SOURCES
-#
-## sync to cache and repo
-#rsync -a pkgs/${PACKNAME}*.el7.centos.x86_64.rpm ${CACHEDIR}
-#rsync -a pkgs/${PACKNAME}*.rpm ${REPODIR}
-#rm -rf pkgs
+# Now it is time to create the source rpm
+/usr/bin/mock -r default --resultdir=pkgs --buildsrpm --spec=${PACKNAME}.spec --sources=SOURCES
+
+# with it, we can create rest of packages
+/usr/bin/mock -r default --resultdir=pkgs --rebuild pkgs/${PACKNAME}*.src.rpm
+
+# cleaning
+rm -rf SOURCES
+
+# sync to cache and repo
+rsync -a pkgs/${PACKNAME}*.el7.centos.x86_64.rpm ${CACHEDIR}
+rsync -a pkgs/${PACKNAME}*.rpm ${REPODIR}
+rm -rf pkgs
 
---
+++ modules/librb-http/librb-http.spec
@@ -0,0 +1,69 @@
+%global libname      librb-http
+%global gh_commit    b5dc6128058c53b9be0f8244db143cd34cfb32fe
+%global gh_short     %(c=%{gh_commit}; echo ${c:0:7})
+%global gh_owner     bigomby
+%global gh_project   %{libname}
+
+Name:    librb-http
+Summary: Rapid Development C library
+Version: 1.2.0
+Release: 1%{?dist}
+%define soname 0
+
+License: BSD-2-Clause
+URL: https://gitlab.redborder.lan/core-developers/librb-http/repository/archive.tar.gz?ref=1.2.0
+Source0: %{gh_project}-%{version}-%{gh_short}.tar.gz
+#Source0: https://github.com/%{gh_owner}/%{gh_project}/archive/%{gh_commit}/%{gh_project}-%{version}-%{gh_short}.tar.gz
+
+BuildRequires: gcc librd-devel
+
+Summary: Non-blocking high-level wrapper for libcurl
+Group:   Development/Libraries/C and C++
+%description
+%{summary}
+
+%package -n %{name}%{soname}
+Summary: Non-blocking high-level wrapper for libcurl
+Group:   Development/Libraries/C and C++
+Requires: librd
+%description -n %{name}%{soname}
+%{summary}.
+
+%package -n %{name}-devel
+Summary: Development files for %{name}
+Group:   Development/Libraries/C and C++
+Requires: %{name}%{soname} = %{version}-%{release}
+%description -n %{name}-devel
+%{summary}.
+
+%prep
+%setup -qn %{gh_project}-%{version}-%{gh_commit}
+
+%build
+./configure --prefix=/usr
+make
+
+%install
+DESTDIR=%{buildroot} make install
+
+%clean
+rm -rf %{buildroot}
+
+%post -p /sbin/ldconfig
+%postun -p /sbin/ldconfig
+
+%files -n %{name}%{soname}
+%defattr(444,root,root)
+/usr/lib/librbhttp.so
+/usr/lib/librbhttp.so.1
+
+%files -n %{name}-devel
+%defattr(-,root,root)
+%{_includedir}/librbhttp
+/usr/lib/librbhttp.a
+
+%changelog
+* Tue May 10 2016 Juan J. Prieto <jjprieto@redborder.com> - 1.2.0-1
+- first spec version
+
+
