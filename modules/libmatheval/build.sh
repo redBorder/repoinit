287d4916f9e0a2e3cedc1bb901247e1bd140f22f
Juan J. Prieto <root@sdk7.redborder.lan>
2016-05-11 10:28:42 +0200

    added libmatheval module

---
+++ modules/libmatheval/build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+source build_common.sh
+
+VERSION=${VERSION:="1.1.11"}
+PACKNAME=${PACKNAME:="libmatheval"}
+CACHEDIR=${CACHEDIR:="/tmp/sdk7_cache/custom_rpms"}
+REPODIR=${REPODIR:="/tmp/sdk7_repo"}
+VSHORT=$(c=${COMMIT}; echo ${c:0:7})
+
+# First we need to download source
+mkdir SOURCES
+wget http://ftp.gnu.org/gnu/${PACKNAME}/${PACKNAME}-${VERSION}.tar.gz -O SOURCES/${PACKNAME}-${VERSION}.tar.gz
+
+# Now it is time to create the source rpm
+/usr/bin/mock -r default --resultdir=pkgs --buildsrpm --spec=${PACKNAME}.spec --sources=SOURCES
+
+# with it, we can create rest of packages
+/usr/bin/mock -r default --resultdir=pkgs --rebuild pkgs/${PACKNAME}*.src.rpm
+
+# cleaning
+rm -rf SOURCES
+
+# sync to cache and repo
+rsync -a pkgs/${PACKNAME}*.el7.centos.x86_64.rpm ${CACHEDIR}
+rsync -a pkgs/${PACKNAME}*.rpm ${REPODIR}
+rm -rf pkgs
+
+# Update sdk7 repo
+f_updaterepo ${REPODIR}
+
