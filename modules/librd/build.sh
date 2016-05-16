ae0edd122f4509a6a34804165aae63412220cfba
Juan J. Prieto <root@sdk7.redborder.lan>
2016-05-16 03:46:32 +0200

    added check support

--- modules/librd/build.sh
+++ modules/librd/build.sh
@@ -5,26 +5,49 @@ source build_common.sh
 COMMIT=${COMMIT:="3a0441ca2123b3f89d879669b7ca9c047e62c8a8"}
 VERSION=${VERSION:="0.1.0"}
 RELEASE=${RELEASE:="1"}
+LIBVER=${LIBVER:="0"}
 PACKNAME=${PACKNAME:="librd"}
 CACHEDIR=${CACHEDIR:="/tmp/sdk7_cache/custom_rpms"}
 REPODIR=${REPODIR:="/tmp/sdk7_repo"}
 VSHORT=$(c=${COMMIT}; echo ${c:0:7})
+
+list_of_packages="${REPODIR}/${PACKNAME}-${VERSION}-${RELEASE}.el7.centos.src.rpm 
+                ${REPODIR}/${PACKNAME}${LIBVER}-${VERSION}-${RELEASE}.el7.centos.x86_64.rpm 
+                ${REPODIR}/${PACKNAME}-debuginfo-${VERSION}-${RELEASE}.el7.centos.x86_64.rpm
+                ${REPODIR}/${PACKNAME}-devel-${VERSION}-${RELEASE}.el7.centos.x86_64.rpm 
+                ${CACHEDIR}/${PACKNAME}${LIBVER}-${VERSION}-${RELEASE}.el7.centos.x86_64.rpm"
+
+if [ "x$1" != "xforce" ]; then
+        f_check "${list_of_packages}"
+        if [ $? -eq 0 ]; then
+                # the rpms exist and we don't need to create again
+                exit 0
+        fi
+fi
 
 # First we need to download source
 mkdir SOURCES
 wget https://github.com/edenhill/${PACKNAME}/archive/${COMMIT}/${PACKNAME}-${VERSION}-${VSHORT}.tar.gz -O SOURCES/${PACKNAME}-${VERSION}-${VSHORT}.tar.gz
 
 # Now it is time to create the source rpm
-/usr/bin/mock -r sdk7 --resultdir=pkgs --buildsrpm --spec=${PACKNAME}.spec --sources=SOURCES
+/usr/bin/mock -r sdk7 \
+        --define "__version ${VERSION}" \
+        --define "__release ${RELEASE}" \
+        --define "__libver ${LIBVER}" \
+	--resultdir=pkgs --buildsrpm --spec=${PACKNAME}.spec --sources=SOURCES
 
 # with it, we can create rest of packages
-/usr/bin/mock -r sdk7 --resultdir=pkgs --rebuild pkgs/${PACKNAME}*.src.rpm
+/usr/bin/mock -r sdk7 \
+        --define "__version ${VERSION}" \
+        --define "__release ${RELEASE}" \
+        --define "__libver ${LIBVER}" \
+	--resultdir=pkgs --rebuild pkgs/${PACKNAME}*.src.rpm
 
 # cleaning
 rm -rf SOURCES
 
 # sync to cache and repo
-rsync -a pkgs/${PACKNAME}*.el7.centos.x86_64.rpm ${CACHEDIR}
+rsync -a pkgs/${PACKNAME}${LIBVER}-${VERSION}-${RELEASE}.el7.centos.x86_64.rpm ${CACHEDIR}
 rsync -a pkgs/${PACKNAME}*.rpm ${REPODIR}
 rm -rf pkgs
 
--- modules/rb_monitor/build.sh
+++ modules/rb_monitor/build.sh
@@ -4,10 +4,24 @@ source build_common.sh
 
 #COMMIT=${COMMIT:="89fe893f63d4ce0f5fc32c3f8dab75fa94497d08"}
 VERSION=${VERSION:="1.0"}
+RELEASE=${RELEASE:="1"}
 PACKNAME=${PACKNAME:="rb_monitor"}
 CACHEDIR=${CACHEDIR:="/tmp/sdk7_cache/custom_rpms"}
 REPODIR=${REPODIR:="/tmp/sdk7_repo"}
 #VSHORT=$(c=${COMMIT}; echo ${c:0:7})
+
+list_of_packages="${REPODIR}/${PACKNAME}-${VERSION}-${RELEASE}.el7.centos.src.rpm 
+                ${REPODIR}/${PACKNAME}-${VERSION}-${RELEASE}.el7.centos.x86_64.rpm 
+                ${REPODIR}/${PACKNAME}-debuginfo-${VERSION}-${RELEASE}.el7.centos.x86_64.rpm
+                ${CACHEDIR}/${PACKNAME}-${VERSION}-${RELEASE}.el7.centos.x86_64.rpm"
+
+if [ "x$1" != "xforce" ]; then
+        f_check "${list_of_packages}"
+        if [ $? -eq 0 ]; then
+                # the rpms exist and we don't need to create again
+                exit 0
+        fi
+fi
 
 # First we need to download source
 mkdir SOURCES
@@ -16,16 +30,22 @@ cp patches/*.patch SOURCES
 
 
 # Now it is time to create the source rpm
-/usr/bin/mock -r sdk7 --resultdir=pkgs --buildsrpm --spec=${PACKNAME}.spec --sources=SOURCES
+/usr/bin/mock -r sdk7 \
+        --define "__version ${VERSION}" \
+        --define "__release ${RELEASE}" \
+	--resultdir=pkgs --buildsrpm --spec=${PACKNAME}.spec --sources=SOURCES
 
 # with it, we can create rest of packages
-/usr/bin/mock -r sdk7 --resultdir=pkgs --rebuild pkgs/${PACKNAME}*.src.rpm
+/usr/bin/mock -r sdk7 \
+        --define "__version ${VERSION}" \
+        --define "__release ${RELEASE}" \
+	--resultdir=pkgs --rebuild pkgs/${PACKNAME}*.src.rpm
 
 # cleaning
 rm -rf SOURCES
 
 # sync to cache and repo
-rsync -a pkgs/${PACKNAME}*.el7.centos.x86_64.rpm ${CACHEDIR}
+rsync -a pkgs/${PACKNAME}-${VERSION}-${RELEASE}.el7.centos.x86_64.rpm ${CACHEDIR}
 rsync -a pkgs/${PACKNAME}*.rpm ${REPODIR}
 rm -rf pkgs
 
