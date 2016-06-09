fd257de71fffb109b71f85ef0150a0f789a595bf
Juan J. Prieto <root@sdk7.redborder.lan>
2016-06-09 04:43:55 +0200

    deleted uneeded modules

--- modules/nprobe/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# First we need to download source
-mkdir SOURCES
-wget --no-check-certificate https://gitlab.redborder.lan/redborder/nprobe/repository/archive.tar.gz?ref=master --header='PRIVATE-TOKEN:oDRezN5gFLgBB6nWsMZU' -O SOURCES/nprobe.tar.gz
-
-
