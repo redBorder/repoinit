10486910b3d2e5c3cdccdf4d561414b201848d74
Juan J. Prieto <root@sdk7.redborder.lan>
2016-07-29 04:21:30 +0200

    disable auto-update rbrepo

--- modules/chef-client/build.sh
+++ modules/chef-client/build.sh
@@ -8,26 +8,26 @@ PACKNAME=${PACKNAME:="chef"}
 CACHEDIR=${CACHEDIR:="/isos/redBorder"}
 REPODIR=${REPODIR:="/repos/redBorder"}
 
-list_of_packages="${REPODIR}/${PACKNAME}-${VERSION}-${RELEASE}.el7.x86_64.rpm 
-                ${CACHEDIR}/${PACKNAME}-${VERSION}-${RELEASE}.el7.x86_64.rpm"
-
-if [ "x$1" != "xforce" ]; then
-        f_check "${list_of_packages}"
-        if [ $? -eq 0 ]; then
-                # the rpms exist and we don't need to create again
-                exit 0
-        fi
-fi
+#list_of_packages="${REPODIR}/${PACKNAME}-${VERSION}-${RELEASE}.el7.x86_64.rpm 
+#                ${CACHEDIR}/${PACKNAME}-${VERSION}-${RELEASE}.el7.x86_64.rpm"
+#
+#if [ "x$1" != "xforce" ]; then
+#        f_check "${list_of_packages}"
+#        if [ $? -eq 0 ]; then
+#                # the rpms exist and we don't need to create again
+#                exit 0
+#        fi
+#fi
 
 # First we need to download source
 mkdir pkgs
 wget https://packages.chef.io/stable/el/7/${PACKNAME}-${VERSION}-${RELEASE}.el7.x86_64.rpm -O pkgs/${PACKNAME}-${VERSION}-${RELEASE}.el7.x86_64.rpm
 
 # sync to cache and repo
-f_rsync_repo pkgs/${PACKNAME}*.rpm
-f_rsync_iso pkgs/${PACKNAME}*.rpm
-rm -rf pkgs
+#f_rsync_repo pkgs/${PACKNAME}*.rpm
+#f_rsync_iso pkgs/${PACKNAME}*.rpm
+#rm -rf pkgs
 
 # Update sdk7 repo
-f_rupdaterepo ${REPODIR}
+#f_rupdaterepo ${REPODIR}
 
