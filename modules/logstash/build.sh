514be31639d4b500161a4438fa671a37865a2a96
Miguel Negrón <manegron@redborder.com>
2021-11-23 17:08:32 +0100

    Fix logstash module

--- modules/logstash/build.sh
+++ modules/logstash/build.sh
@@ -4,13 +4,12 @@ source build_common.sh
 
 MAJOR_VERSION=${MAJOR_VERSION:="2.1"}
 VERSION=${VERSION:="7.15.2"}
-RELEASE=${RELEASE:="1"}
 PACKNAME=${PACKNAME:="logstash"}
 CACHEDIR=${CACHEDIR:="/isos/redBorder"}
 REPODIR=${REPODIR:="/repos/redBorder"}
 
-list_of_packages="${REPODIR}/${PACKNAME}-${VERSION}-${RELEASE}.el7.centos.x86_64.rpm 
-                ${CACHEDIR}/${PACKNAME}-${VERSION}-${RELEASE}.el7.centos.x86_64.rpm"
+list_of_packages="${REPODIR}/${PACKNAME}-${VERSION}-x86_64.rpm 
+                ${CACHEDIR}/${PACKNAME}-${VERSION}-x86_64.rpm"
 
 if [ "x$1" != "xforce" ]; then
         f_check "${list_of_packages}"
