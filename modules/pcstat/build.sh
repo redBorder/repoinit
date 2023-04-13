77afe7d37601a465f770a579cb7cacce3e9afe2a
Miguel <manegron@redborder.com>
2023-04-13 15:20:48 +0100

    Add pcstat

---
+++ modules/pcstat/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+source build_common.sh
+
+VERSION=${VERSION:="0.0.1"}
+RELEASE=${RELEASE:="1"}
+PACKNAME=${PACKNAME:="pcstat"}
+
+# First we need to download source
+mkdir SOURCES
+wget https://github.com/tobert/pcstat/releases/download/v0.0.1/pcstat_0.0.1_x86_64.rpm
+
+# sync to cache and repo
+f_rsync_repo pkgs/${PACKNAME}*.rpm
+f_rsync_iso pkgs/${PACKNAME}*.rpm
+rm -rf pkgs
+
+# Update sdk7 repo
+f_rupdaterepo ${REPODIR}
