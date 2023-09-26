73fbfd52ce2626da8d142b6664eb6168156c2b23
David Vanhoucke <dvanhoucke@redborder.com>
2023-09-26 14:31:18 +0100

    logstash module

--- modules/logstash/build.sh
+++ modules/logstash/build.sh
@@ -5,8 +5,9 @@ source build_common.sh
 MAJOR_VERSION=${MAJOR_VERSION:="2.1"}
 VERSION=${VERSION:="7.15.2"}
 PACKNAME=${PACKNAME:="logstash"}
-CACHEDIR=${CACHEDIR:="/isos/redBorder"}
-REPODIR=${REPODIR:="/repos/redBorder"}
+CACHEDIR=${CACHEDIR:="/isos/ng/latest/rhel/9/x86_64"}
+REPODIR=${REPODIR:="/repos/ng/latest/rhel/9/x86_64"}
+REPODIR_SRPMS=${REPODIR_SRPMS:="/repos/ng/latest/rhel/9/SRPMS"}
 
 list_of_packages="${REPODIR}/${PACKNAME}-${VERSION}-x86_64.rpm 
                 ${CACHEDIR}/${PACKNAME}-${VERSION}-x86_64.rpm"
@@ -21,11 +22,12 @@ fi
 
 # First we need to download source
 mkdir pkgs
-wget https://artifacts.elastic.co/downloads/logstash/${PACKNAME}-${VERSION}-x86_64.rpm -O pkgs/${PACKNAME}-${VERSION}-${RELEASE}-x86_64.rpm
+wget https://artifacts.elastic.co/downloads/logstash/${PACKNAME}-${VERSION}-x86_64.rpm -O pkgs/${PACKNAME}-${VERSION}-x86_64.rpm
 
 # sync to cache and repo
 f_rsync_repo pkgs/${PACKNAME}*.rpm
 f_rsync_iso pkgs/${PACKNAME}*.rpm
+
 rm -rf pkgs
 
 # Update sdk9 repo
