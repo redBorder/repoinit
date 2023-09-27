b545e842ee7668d0fdad98b505a18bd475687244
David Vanhoucke <dvanhoucke@redborder.com>
2023-09-27 15:28:48 +0100

    module libmatheval

--- modules/libmatheval/build.sh
+++ modules/libmatheval/build.sh
@@ -5,14 +5,15 @@ source build_common.sh
 VERSION=${VERSION:="1.1.11"}
 RELEASE=${RELEASE:="3"}
 PACKNAME=${PACKNAME:="libmatheval"}
-CACHEDIR=${CACHEDIR:="/isos/redBorder"}
-REPODIR=${REPODIR:="/repos/redBorder"}
+CACHEDIR=${CACHEDIR:="/isos/ng/latest/rhel/9/x86_64"}
+REPODIR=${REPODIR:="/repos/ng/latest/rhel/9/x86_64"}
+REPODIR_SRPMS=${REPODIR_SRPMS:="/repos/ng/latest/rhel/9/SRPMS"}
 
-list_of_packages="${REPODIR}/${PACKNAME}-${VERSION}-${RELEASE}.el7.centos.src.rpm 
-                ${REPODIR}/${PACKNAME}-${VERSION}-${RELEASE}.el7.centos.x86_64.rpm 
-                ${REPODIR}/${PACKNAME}-debuginfo-${VERSION}-${RELEASE}.el7.centos.x86_64.rpm
-                ${REPODIR}/${PACKNAME}-devel-${VERSION}-${RELEASE}.el7.centos.x86_64.rpm 
-                ${CACHEDIR}/${PACKNAME}-${VERSION}-${RELEASE}.el7.centos.x86_64.rpm" 
+list_of_packages="${REPODIR_SRPMS}/${PACKNAME}-${VERSION}-${RELEASE}.el7.rb.src.rpm 
+                ${REPODIR}/${PACKNAME}-${VERSION}-${RELEASE}.el7.rb.x86_64.rpm 
+                ${REPODIR}/${PACKNAME}-debuginfo-${VERSION}-${RELEASE}.el7.rb.x86_64.rpm
+                ${REPODIR}/${PACKNAME}-devel-${VERSION}-${RELEASE}.el7.rb.x86_64.rpm 
+                ${CACHEDIR}/${PACKNAME}-${VERSION}-${RELEASE}.el7.rb.x86_64.rpm" 
 
 if [ "x$1" != "xforce" ]; then
         f_check "${list_of_packages}"
@@ -23,38 +24,44 @@ if [ "x$1" != "xforce" ]; then
 fi
 
 # First we need to download source
+rm -rf SOURCES
+rm -rf pkgs
 mkdir SOURCES
+mkdir pkgs
 wget http://ftp.gnu.org/gnu/${PACKNAME}/${PACKNAME}-${VERSION}.tar.gz -O SOURCES/${PACKNAME}-${VERSION}.tar.gz
+ret=$?
+if [ $ret -ne 0 ]; then
+        echo "Error in getting ${PACKNAME}/${PACKNAME}-${VERSION}.tar.gz... exiting"
+        exit 1
+fi
 
 # Now it is time to create the source rpm
-/usr/bin/mock -r sdk9 \
+/usr/bin/mock -r sdk7 \
         --define "__version ${VERSION}" \
         --define "__release ${RELEASE}" \
 	--resultdir=pkgs --buildsrpm --spec=${PACKNAME}.spec --sources=SOURCES
 
 # with it, we can create rest of packages
-/usr/bin/mock -r sdk9 \
+/usr/bin/mock -r sdk7 \
         --define "__version ${VERSION}" \
         --define "__release ${RELEASE}" \
 	--resultdir=pkgs --rebuild pkgs/${PACKNAME}*.src.rpm
 
 ret=$?
-
-# cleaning
-rm -rf SOURCES
-
 if [ $ret -ne 0 ]; then
         echo "Error in mock stage ... exiting"
         exit 1
 fi
 
 # sync to cache and repo
-#rsync -a pkgs/${PACKNAME}-${VERSION}-${RELEASE}.el7.centos.x86_64.rpm ${CACHEDIR}
-#rsync -a pkgs/${PACKNAME}*.rpm ${REPODIR}
-f_rsync_iso pkgs/${PACKNAME}-${VERSION}-${RELEASE}.el7.centos.x86_64.rpm
-f_rsync_repo pkgs/${PACKNAME}*.rpm
+f_rsync_repo pkgs/*.x86_64.rpm
+f_rsync_repo_SRPMS pkgs/*.src.rpm
+f_rsync_iso pkgs/*.x86_64.rpm
+
 rm -rf pkgs
+rm -rf SOURCES
 
 # Update sdk9 repo
-f_rupdaterepo
+f_rupdaterepo ${REPODIR}
+f_rupdaterepo $REPODIR_SRPMS
 
