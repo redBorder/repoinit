24c72e87b4978c4a4ff8aeb1b322610394c6a548
David Vanhoucke <dvanhoucke@redborder.com>
2023-09-29 16:30:21 +0100

    module libcdada

---
+++ modules/libcdada/build.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+source build_common.sh
+
+VERSION=${VERSION:="0.5.1"}
+RELEASE=${RELEASE:="1"}
+PACKNAME=${PACKNAME:="libcdada"}
+CACHEDIR=${CACHEDIR:="/isos/ng/latest/rhel/9/x86_64"}
+REPODIR=${REPODIR:="/repos/ng/latest/rhel/9/x86_64"}
+REPODIR_SRPMS=${REPODIR_SRPMS:="/repos/ng/latest/rhel/9/SRPMS"}
+
+list_of_packages="${REPODIR_SRPMS}/${PACKNAME}-${VERSION}-${RELEASE}.el9.rb.src.rpm 
+                ${REPODIR}/${PACKNAME}0-${VERSION}-${RELEASE}.el9.rb.x86_64.rpm 
+                ${REPODIR}/${PACKNAME}-devel-${VERSION}-${RELEASE}.el9.rb.x86_64.rpm
+                ${REPODIR}/${PACKNAME}0-debuginfo-${VERSION}-${RELEASE}.el9.rb.x86_64.rpm
+                ${REPODIR}/${PACKNAME}-debugsource-${VERSION}-${RELEASE}.el9.rb.x86_64.rpm
+                ${CACHEDIR}/${PACKNAME}0-${VERSION}-${RELEASE}.el9.rb.x86_64.rpm 
+                ${CACHEDIR}/${PACKNAME}-devel-${VERSION}-${RELEASE}.el9.rb.x86_64.rpm
+                ${CACHEDIR}/${PACKNAME}0-debuginfo-${VERSION}-${RELEASE}.el9.rb.x86_64.rpm
+                ${CACHEDIR}/${PACKNAME}-debugsource-${VERSION}-${RELEASE}.el9.rb.x86_64.rpm"
+
+if [ "x$1" != "xforce" ]; then
+        f_check "${list_of_packages}"
+        if [ $? -eq 0 ]; then
+                # the rpms exist and we don't need to create again
+                exit 0
+        fi
+fi
+
+# First we need to download source
+rm -rf SOURCES
+rm -rf pkgs
+mkdir pkgs
+mkdir SOURCES
+wget https://github.com/msune/libcdada/archive/refs/tags/v${VERSION}.tar.gz -O SOURCES/${PACKNAME}-${VERSION}.tar.gz
+
+ret=$?
+if [ $ret -ne 0 ]; then
+        echo "Error in getting lcdada tar... exiting"
+        exit 1
+fi
+
+# Now it is time to create the source rpm
+/usr/bin/mock -r sdk9 \
+        --define "__version ${VERSION}" \
+        --define "__release ${RELEASE}" \
+	--resultdir=pkgs --buildsrpm --spec=${PACKNAME}.spec --sources=SOURCES 
+
+# with it, we can create rest of packages
+/usr/bin/mock -r sdk9 \
+        --define "__version ${VERSION}" \
+        --define "__release ${RELEASE}" \
+	--resultdir=pkgs --rebuild pkgs/${PACKNAME}*.src.rpm
+
+
+ret=$?
+if [ $ret -ne 0 ]; then
+        echo "Error in mock stage ... exiting"
+        exit 1
+fi
+
+# sync to cache and repo
+f_rsync_repo pkgs/*.x86_64.rpm
+f_rsync_repo_SRPMS pkgs/*.src.rpm
+f_rsync_iso pkgs/*.x86_64.rpm
+
+rm -rf pkgs
+rm -rf SOURCES
+
+# Update sdk9 repo
+f_rupdaterepo ${REPODIR}
+f_rupdaterepo ${REPODIR_SRPMS}
