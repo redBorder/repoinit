f2b97df25a04092f254f655c53c1b1d4b6ae17a2
David Vanhoucke <dvanhoucke@redborder.com>
2023-10-04 09:53:55 +0000

    module minio

--- modules/minio/build.sh
+++ modules/minio/build.sh
@@ -6,12 +6,13 @@ VERSION=${VERSION:="0.1.1"}
 RELEASE=${RELEASE:="2"}
 MINIORELEASE=${MINIORELEASE:="20230125001954"}
 PACKNAME=${PACKNAME:="minio"}
-CACHEDIR=${CACHEDIR:="/isos/redBorder"}
-REPODIR=${REPODIR:="/repos/redBorder"}
+CACHEDIR=${CACHEDIR:="/isos/ng/latest/rhel/9/x86_64"}
+REPODIR=${REPODIR:="/repos/ng/latest/rhel/9/x86_64"}
+REPODIR_SRPMS=${REPODIR_SRPMS:="/repos/ng/latest/rhel/9/SRPMS"}
 
-list_of_packages="${REPODIR}/${PACKNAME}-${VERSION}-${RELEASE}.el7.rb.src.rpm 
-                ${REPODIR}/${PACKNAME}-${VERSION}-${RELEASE}.el7.rb.x86_64.rpm 
-                ${CACHEDIR}/${PACKNAME}-${VERSION}-${RELEASE}.el7.rb.x86_64.rpm"
+list_of_packages="${REPODIR_SRPMS}/${PACKNAME}-${VERSION}-${RELEASE}.el9.rb.src.rpm 
+                ${REPODIR}/${PACKNAME}-${VERSION}-${RELEASE}.el9.rb.x86_64.rpm 
+                ${CACHEDIR}/${PACKNAME}-${VERSION}-${RELEASE}.el9.rb.x86_64.rpm"
 
 if [ "x$1" != "xforce" ]; then
         f_check "${list_of_packages}"
@@ -22,7 +23,10 @@ if [ "x$1" != "xforce" ]; then
 fi
 
 # First we need to download source
+rm -rf SOURCES
+rm -rf pkgs
 mkdir SOURCES
+mkdir pkgs
 pushd SOURCES &>/dev/null
 wget --no-check-certificate https://dl.minio.io/server/minio/release/linux-amd64/archive/${PACKNAME}-${MINIORELEASE}.0.0.x86_64.rpm 
 rpm2cpio ${PACKNAME}-${MINIORELEASE}.0.0.x86_64.rpm | cpio -idmv && mv /usr/local/bin/minio .    #Extract the executable and move it to SOURCES
@@ -43,19 +47,20 @@ cp minio.service SOURCES/minio.service
 	--resultdir=pkgs --rebuild pkgs/${PACKNAME}*.src.rpm
 
 ret=$?
-
-# cleaning
-rm -rf SOURCES
-
 if [ $ret -ne 0 ]; then
 	echo "Error in mock stage ... exiting"
 	exit 1
 fi
 
 # sync to cache and repo
-f_rsync_repo pkgs/${PACKNAME}*.rpm
-f_rsync_iso pkgs/${PACKNAME}-${VERSION}-${RELEASE}.el7.rb.x86_64.rpm
+f_rsync_repo pkgs/*.x86_64.rpm
+f_rsync_repo_SRPMS pkgs/*.src.rpm
+f_rsync_iso pkgs/*.x86_64.rpm
+
 rm -rf pkgs
+rm -rf SOURCES
 
 # Update sdk9 repo
 f_rupdaterepo ${REPODIR}
+f_rupdaterepo ${REPODIR_SRPMS}
+
