954296ef557ba633b59c3d25c8a2891185d8e54b
Miguel Negron <manegron@redborder.com>
2024-10-15 13:02:12 +0100

    Add mc

---
+++ modules/mc/build.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+source build_common.sh
+
+VERSION=${VERSION:="0.0.1"}
+RELEASE=${RELEASE:="1"}
+PACKNAME=${PACKNAME:="mc"}
+CACHEDIR=${CACHEDIR:="/isos/ng/latest/rhel/9/x86_64"}
+REPODIR=${REPODIR:="/repos/ng/latest/rhel/9/x86_64"}
+REPODIR_SRPMS=${REPODIR_SRPMS:="/repos/ng/latest/rhel/9/SRPMS"}
+
+list_of_packages="${REPODIR_SRPMS}/${PACKNAME}-${VERSION}-${RELEASE}.el9.rb.src.rpm 
+                ${REPODIR}/${PACKNAME}-${VERSION}-${RELEASE}.el9.rb.x86_64.rpm 
+                ${REPODIR}/${PACKNAME}-debuginfo-${VERSION}-${RELEASE}.el9.rb.x86_64.rpm
+                ${CACHEDIR}/${PACKNAME}-${VERSION}-${RELEASE}.el9.rb.x86_64.rpm"
+
+if [ "x$1" != "xforce" ]; then
+        f_check "${list_of_packages}"
+        if [ $? -eq 0 ]; then
+                # the rpms exist and we don't need to create again
+                exit 0
+        fi
+fi
+
+# First we need to download source
+rm -rf SOURCES
+rm -rf pkgs
+mkdir SOURCES
+mkdir pkgs
+wget https://dl.min.io/client/mc/release/linux-amd64/${PACKNAME} -O SOURCES/${PACKNAME}
+ret=$?
+if [ $ret -ne 0 ]; then
+        echo "Error in getting mc... exiting"
+        exit 1
+fi
+
+# Now it is time to create the source rpm
+/usr/bin/mock -r sdk9 \
+	--define "__version ${VERSION}" \
+	--define "__release ${RELEASE}" \
+	--resultdir=pkgs --buildsrpm --spec=${PACKNAME}.spec --sources=SOURCES
+
+# with it, we can create rest of packages
+/usr/bin/mock -r sdk9 \
+	--define "__version ${VERSION}" \
+	--define "__release ${RELEASE}" \
+	--resultdir=pkgs --rebuild pkgs/${PACKNAME}*.src.rpm
+
+ret=$?
+
+# cleaning
+rm -rf SOURCES
+
+if [ $ret -ne 0 ]; then
+        echo "Error in mock stage ... exiting"
+        exit 1
+fi
+
+# sync to cache and repo
+f_rsync_repo pkgs/*.noarch.rpm
+f_rsync_repo_SRPMS pkgs/*.src.rpm
+f_rsync_iso pkgs/*.noarch.rpm
+
+rm -rf pkgs
+
+# Update sdk9 repo
+f_rupdaterepo $REPODIR
+f_rupdaterepo $REPODIR_SRPMS
+
