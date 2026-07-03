f9561116d35ccdb4b6dc6e38580a9f4caf9e0b2b
Nils <nverschaeve@redborder.com>
2026-07-03 10:55:22 +0100

    fix build.sh

--- modules/govc/build.sh
+++ modules/govc/build.sh
@@ -1,25 +1,8 @@
 #!/bin/bash
 
-source build_common.sh
-
 VERSION=${VERSION:="0.54.1"}
 RELEASE=${RELEASE:="1"}
 PACKNAME=${PACKNAME:="govc"}
-CACHEDIR=${CACHEDIR:="/isos/ng/latest/rhel/9/x86_64"}
-REPODIR=${REPODIR:="/repos/ng/latest/rhel/9/x86_64"}
-REPODIR_SRPMS=${REPODIR_SRPMS:="/repos/ng/latest/rhel/9/SRPMS"}
-
-list_of_packages="${REPODIR_SRPMS}/${PACKNAME}-${VERSION}-${RELEASE}.el9.rb.src.rpm
-                ${REPODIR}/${PACKNAME}-${VERSION}-${RELEASE}.el9.rb.x86_64.rpm
-                ${CACHEDIR}/${PACKNAME}-${VERSION}-${RELEASE}.el9.rb.x86_64.rpm"
-
-if [ "x$1" != "xforce" ]; then
-        f_check "${list_of_packages}"
-        if [ $? -eq 0 ]; then
-                # the rpms exist and we don't need to create again
-                exit 0
-        fi
-fi
 
 # First we need to download source
 rm -rf SOURCES
@@ -56,14 +39,5 @@ if [ $ret -ne 0 ]; then
         exit 1
 fi
 
-# sync to cache and repo
-f_rsync_repo pkgs/*.x86_64.rpm
-f_rsync_repo_SRPMS pkgs/*.src.rpm
-f_rsync_iso pkgs/*.x86_64.rpm
-
-rm -rf pkgs
+# Clean up build sources, but keep build results in pkgs/
 rm -rf SOURCES
-
-# Update sdk9 repo
-f_rupdaterepo ${REPODIR}
-f_rupdaterepo ${REPODIR_SRPMS}
