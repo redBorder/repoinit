11be3c8b45df576490d5939718f4575d99041c10
Nils <nverschaeve@redborder.com>
2026-07-03 10:45:17 +0100

    add govc module for rpm packaging

---
+++ modules/govc/build.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+source build_common.sh
+
+VERSION=${VERSION:="0.54.1"}
+RELEASE=${RELEASE:="1"}
+PACKNAME=${PACKNAME:="govc"}
+CACHEDIR=${CACHEDIR:="/isos/ng/latest/rhel/9/x86_64"}
+REPODIR=${REPODIR:="/repos/ng/latest/rhel/9/x86_64"}
+REPODIR_SRPMS=${REPODIR_SRPMS:="/repos/ng/latest/rhel/9/SRPMS"}
+
+list_of_packages="${REPODIR_SRPMS}/${PACKNAME}-${VERSION}-${RELEASE}.el9.rb.src.rpm
+                ${REPODIR}/${PACKNAME}-${VERSION}-${RELEASE}.el9.rb.x86_64.rpm
+                ${CACHEDIR}/${PACKNAME}-${VERSION}-${RELEASE}.el9.rb.x86_64.rpm"
+
+if [ "x$1" != "xforce" ]; then
+        f_check "${list_of_packages}"
+        if [ $? -eq 0 ]; then
+                # the rpms exist and we don't need to create again
+                exit 0
+        fi
+fi
+
+# First we need to download source
+rm -rf SOURCES
+rm -rf pkgs
+mkdir SOURCES
+mkdir pkgs
+wget --no-check-certificate https://github.com/vmware/govmomi/releases/download/v${VERSION}/govc_Linux_x86_64.tar.gz -O SOURCES/govc_Linux_x86_64.tar.gz
+ret=$?
+if [ $ret -ne 0 ]; then
+        echo "Error in getting govc_Linux_x86_64.tar.gz... exiting"
+        exit 1
+fi
+
+pushd SOURCES &>/dev/null
+tar -xzf govc_Linux_x86_64.tar.gz
+rm -f govc_Linux_x86_64.tar.gz
+popd &>/dev/null
+
+# Now it is time to create the source rpm
+/usr/bin/mock -r sdk9 \
+        --define "__version ${VERSION}" \
+        --define "__release ${RELEASE}" \
+        --resultdir=pkgs --buildsrpm --spec=${PACKNAME}.spec --sources=SOURCES
+
+# with it, we can create rest of packages
+/usr/bin/mock -r sdk9 \
+        --define "__version ${VERSION}" \
+        --define "__release ${RELEASE}" \
+        --resultdir=pkgs --rebuild pkgs/${PACKNAME}*.src.rpm
+
+ret=$?
+if [ $ret -ne 0 ]; then
+        echo "Error in mock stage ... exiting"
+        exit 1
+fi
+
+# sync to cache and repo
+f_rsync_repo pkgs/*.x86_64.rpm
+f_rsync_repo_SRPMS pkgs/*.src.rpm
+f_rsync_iso pkgs/*.x86_64.rpm
+
+rm -rf pkgs
+rm -rf SOURCES
+
+# Update sdk9 repo
+f_rupdaterepo ${REPODIR}
+f_rupdaterepo ${REPODIR_SRPMS}
---
+++ modules/govc/govc.spec
@@ -0,0 +1,28 @@
+Name: govc
+Version: %{__version}
+Release: %{__release}%{?dist}
+Summary: vSphere CLI built on top of govmomi
+
+Group: System Environment/Base
+License: MIT
+URL: https://github.com/vmware/govmomi
+Source0: govc
+
+%description
+govc is a vSphere CLI built on top of govmomi.
+
+%prep
+
+%build
+
+%install
+mkdir -p %{buildroot}%{_bindir}
+install -D -m 755 %{SOURCE0} %{buildroot}%{_bindir}/govc
+
+%files
+%defattr(0755,root,root)
+%{_bindir}/govc
+
+%changelog
+* Wed Jun 24 2026 Nils <nverschaeve@redborder.com> - 0.54.1-1
+- first spec version
